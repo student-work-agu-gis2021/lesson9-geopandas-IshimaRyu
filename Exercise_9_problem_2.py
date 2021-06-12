a64726d80c980ccacc2590469abf870f9304c4a0
IshimaRyu <82187608+IshimaRyu@users.noreply.github.com>
2021-06-12 05:53:20 +0000

    I saved the data into a Shapefile called Kruger_posts.shp

--- Exercise_9_problem_2.py
+++ Exercise_9_problem_2.py
@@ -35,6 +35,8 @@ from pyproj import CRS
 
 # Convert DataFrame into a GeoDataFrame
 geo= gpd.GeoDataFrame(data, geometry='geometry',crs=CRS.from_epsg(4326).to_wkt())
+fp="Kruger_posts.shp"
+geo.to_file(fp)
 # CODE FOR TESTING YOUR SOLUTION
 
 # Check the geodataframe head
